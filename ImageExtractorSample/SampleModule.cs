266dc616d4fbaa9dd4eb8dc709e812ec5b178256
Andre Lafleur <alafleur@GENETEC.COM>
2024-03-19 17:19:36 +0800

    Added ImageExtractorSample project

---
+++ ImageExtractorSample/SampleModule.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C) 2023 by Genetec, Inc. All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+
+namespace Genetec.Dap.CodeSamples
+{
+    using System.Diagnostics;
+    using Sdk.Workspace.Modules;
+
+    public class SampleModule : Module
+    {
+        public override void Load()
+        {
+            var component = new SampleImageExtractor();
+            component.Initialize(Workspace);
+            Workspace.Components.Register(component);
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
