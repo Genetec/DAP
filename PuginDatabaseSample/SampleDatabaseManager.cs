09ad936bf8108bdbc3f1a62fcd270d07e023e86a
Andre Lafleur <alafleur@GENETEC.COM>
2024-04-08 12:33:22 +0800

    Add plugin with database sample

---
+++ PuginDatabaseSample/SampleDatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿namespace Genetec.Dap.CodeSamples
+{
+    using Genetec.Sdk.Plugin;
+
+    public class SampleDatabaseManager : DatabaseManager
+    {
+
+        public DatabaseConfiguration Configuration { get; private set; }
+
+        public override string GetSpecificCreationScript(string databaseName)
+        {
+            
+        }
+
+        public override void SetDatabaseInformation(DatabaseConfiguration databaseConfiguration)
+        {
+            Configuration = Configuration;
+        }
+    }
+}
---
+++ PuginDatabaseSample/SamplePlugin.cs
@@ -0,0 +1,27 @@
+﻿namespace Genetec.Dap.CodeSamples
+{
+    using Sdk.EventsArgs;
+    using Sdk.Plugin;
+    using Sdk.Plugin.Interfaces;
+
+    public class SamplePlugin : Plugin, IPluginDatabaseSupport
+    {
+        public DatabaseManager DatabaseManager { get; } = new SampleDatabaseManager();
+
+        protected override void Dispose(bool disposing)
+        {
+        }
+
+        protected override void OnPluginLoaded()
+        {
+        }
+
+        protected override void OnPluginStart()
+        {
+        }
+
+        protected override void OnQueryReceived(ReportQueryReceivedEventArgs args)
+        {
+        }
+    }
+}
