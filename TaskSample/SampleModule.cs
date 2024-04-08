b68e50e0e9be326e3c1a90e45d38f2217a2b2d83
Andre Lafleur <alafleur@GENETEC.COM>
2024-04-08 12:31:40 +0800

    Add Task sample project

---
+++ TaskSample/SampleModule.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (C) 2023 by Genetec, Inc. All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+
+namespace Genetec.Dap.CodeSamples
+{
+    using Sdk;
+    using Sdk.Workspace.Modules;
+
+    public class SampleModule : Module
+    {
+        public override void Load()
+        {
+            if (Workspace.ApplicationType == ApplicationType.ConfigTool)
+            {
+                var task = new NotepadTask();
+                task.Initialize(Workspace);
+                Workspace.Tasks.Register(task);
+            }
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
---
+++ TaskSample/SampleTask.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (C) 2023 by Genetec, Inc. All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+
+namespace Genetec.Dap.CodeSamples
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Media.Imaging;
+    using Genetec.Sdk;
+    using Genetec.Sdk.Workspace.Tasks;
+
+    public class NotepadTask : Task
+    {
+        public NotepadTask()
+        {
+            Icon = new BitmapImage(new Uri("pack://application:,,,/TaskSample;Component/Resources/Icon.png", UriKind.RelativeOrAbsolute)); 
+            Thumbnail = new BitmapImage(new Uri("pack://application:,,,/TaskSample;Component/Resources/Thumbnail.png", UriKind.RelativeOrAbsolute));
+            Name = "Launch notepad";
+            Description = "Launch Notepad application. This sample illustrates a Task that executes without opening a page in the Security Desk or Config Tool.";
+            CategoryId = new Guid(TaskCategories.Administration);
+        }
+
+        public override bool CanExecute()
+        {
+            return true;
+        }
+
+        public override void Execute()
+        {
+            HideHomePageAfterExecution = false;
+            Process.Start("notepad.exe");
+        }
+    }
+}
