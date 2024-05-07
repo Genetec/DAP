d1a0ebfadad60cdb085dfee2fc683ae5b3189d2b
Andre Lafleur <alafleur@GENETEC.COM>
2024-05-07 10:31:45 +0800

    Created the projet template

---
+++ AccessVerifierSample/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (C) 2023 by Genetec, Inc. All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+
+namespace Genetec.Dap.CodeSamples
+{
+    using System;
+    using System.Threading.Tasks;
+    using Sdk;
+    using Sdk.Entities;
+
+    class Program
+    {
+        static Program() => SdkResolver.Initialize();
+
+        static async Task Main()
+        {
+            const string server = "localhost";
+            const string username = "admin";
+            const string password = "";
+
+            using var engine = new Engine();
+
+            ConnectionStateCode state = await engine.LogOnAsync(server, username, password);
+
+            if (state == ConnectionStateCode.Success)
+            {
+              
+
+            }
+            else
+            {
+                Console.WriteLine($"Logon failed: {state}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    
+
+    }
+}
