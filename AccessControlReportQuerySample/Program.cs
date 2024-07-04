a101a74bf7be18805121c46992c79290158bfaad
Andre Lafleur <alafleur@GENETEC.COM>
2024-07-04 18:44:41 +0800

    Refactor access control query handling
    - Consolidated activity querying into a single `QueryAndDisplayActivities` method, enhancing versatility and reducing code duplication.
    - Introduced a `maxResultCount` parameter to limit query results, improving performance.
    - Created a unified `ExecuteAccessControlReportQuery` method for executing various types of access control report queries, streamlining the process.
    - Simplified data handling by integrating `AccessControlEvent` object creation within the new query execution method, maintaining functionality while reducing complexity.

--- AccessControlReportQuerySample/Program.cs
+++ AccessControlReportQuerySample/Program.cs
@@ -1,7 +1,9 @@
-﻿// Copyright 2024 Genetec
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+﻿// Copyright 2024 Genetec Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 namespace Genetec.Dap.CodeSamples
 {
@@ -10,10 +12,10 @@ namespace Genetec.Dap.CodeSamples
     using System.Data;
     using System.Linq;
     using System.Threading.Tasks;
-    using Sdk;
-    using Sdk.Entities;
-    using Sdk.Queries;
-    using Sdk.Queries.AccessControl;
+    using Genetec.Sdk;
+    using Genetec.Sdk.Entities;
+    using Genetec.Sdk.Queries;
+    using Genetec.Sdk.Queries.AccessControl;
 
     class Program
     {
@@ -31,42 +33,19 @@ namespace Genetec.Dap.CodeSamples
 
             if (state == ConnectionStateCode.Success)
             {
-                DateTime from = DateTime.UtcNow.AddDays(-30);
+                DateTime from = DateTime.UtcNow.AddDays(-30); // last 30 days
                 DateTime to = DateTime.UtcNow;
+                int maxResultCount = 10; // limit the number of results to 10
+                Guid allEntitiesGuid = SystemConfiguration.SystemConfigurationGuid; // Get result from all entities
 
-                Guid allEntitiesGuid = SystemConfiguration.SystemConfigurationGuid;
-
-                List<AccessControlEvent> unitActivities = await GetUnitActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Unit activities ({unitActivities.Count})");
-                DisplayActivities(unitActivities);
-
-                List<AccessControlEvent> credentialActivities = await GetCredentialActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Credential activities ({credentialActivities.Count})");
-                DisplayActivities(credentialActivities);
-
-                List<AccessControlEvent> cardholderActivities = await GetCardholderActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Cardholder activities ({cardholderActivities.Count})");
-                DisplayActivities(cardholderActivities);
-
-                List<AccessControlEvent> areaActivities = await GetAreaActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Area activities ({areaActivities.Count})");
-                DisplayActivities(areaActivities);
-
-                List<AccessControlEvent> doorActivities = await GetDoorActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Door activities ({doorActivities.Count})");
-                DisplayActivities(doorActivities);
-
-                List<AccessControlEvent> zoneActivities = await GetZoneActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Zone activities ({zoneActivities.Count})");
-                DisplayActivities(zoneActivities);
-
-                List<AccessControlEvent> elevatorActivities = await GetElevatorActivities(engine, from, to, allEntitiesGuid);
-                Console.WriteLine($"Elevator activities ({elevatorActivities.Count})");
-                DisplayActivities(elevatorActivities);
-
-                List<AccessControlEvent> visitorActivities = await GetVisitorActivities(engine, allEntitiesGuid, from, to);
-                Console.WriteLine($"Visitor activities ({visitorActivities.Count})");
-                DisplayActivities(visitorActivities);
+                await QueryAndDisplayActivities("Unit", ReportType.AccessControlUnitActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Credential", ReportType.CredentialActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Cardholder", ReportType.CardholderActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Area", ReportType.AreaActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Door", ReportType.DoorActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Zone", ReportType.ZoneActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Elevator", ReportType.ElevatorActivity, from, to, maxResultCount, allEntitiesGuid);
+                await QueryAndDisplayActivities("Visitor", ReportType.VisitorActivity, from, to, maxResultCount, allEntitiesGuid);
             }
             else
             {
@@ -76,8 +55,12 @@ namespace Genetec.Dap.CodeSamples
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 
-            void DisplayActivities(IEnumerable<AccessControlEvent> activities)
+            async Task QueryAndDisplayActivities(string activityType, ReportType reportType, DateTime from, DateTime to,int maximumResultCount, params Guid[] allEntitiesGuid)
             {
+                List<AccessControlEvent> activities = await ExecuteAccessControlReportQuery(reportType, from, to, maximumResultCount, allEntitiesGuid);
+                
+                Console.WriteLine($"{activityType} activities ({activities.Count})");
+
                 foreach (AccessControlEvent activity in activities)
                 {
                     Console.Write($"Timestamp: {activity.Timestamp}, EventType: {activity.EventType}");
@@ -104,88 +87,42 @@ namespace Genetec.Dap.CodeSamples
                     }
                 }
             }
-        }
 
-        static Task<List<AccessControlEvent>> GetCredentialActivities(Engine engine, DateTime from, DateTime to, params Guid[] credentials)
-        {
-            var query = (CredentialActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.CredentialActivity);
-            query.Credentials.AddRange(credentials);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetCardholderActivities(Engine engine, DateTime from, DateTime to, params Guid[] cardholders)
-        {
-            var query = (CardholderActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.CardholderActivity);
-            query.Cardholders.AddRange(cardholders);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetAreaActivities(Engine engine, DateTime from, DateTime to, params Guid[] areas)
-        {
-            var query = (AreaActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.AreaActivity);
-            query.Areas.AddRange(areas);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetDoorActivities(Engine engine, DateTime from, DateTime to, params Guid[] doors)
-        {
-            var query = (DoorActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.DoorActivity);
-            query.Doors.AddRange(doors);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetZoneActivities(Engine engine, DateTime from, DateTime to, params Guid[] zones)
-        {
-            var query = (ZoneActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.ZoneActivity);
-            query.Zones.AddRange(zones);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetElevatorActivities(Engine engine, DateTime from, DateTime to, params Guid[] elevators)
-        {
-            var query = (ElevatorActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.ElevatorActivity);
-            query.Elevators.AddRange(elevators);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetVisitorActivities(Engine engine, Guid visitor, DateTime from, DateTime to)
-        {
-            var query = (VisitorActivityQuery)engine.ReportManager.CreateReportQuery(ReportType.VisitorActivity);
-            query.Visitor = visitor;
-            return ExecuteQuery(query, from, to);
-        }
-
-        static Task<List<AccessControlEvent>> GetUnitActivities(Engine engine, DateTime from, DateTime to, params Guid[] units)
-        {
-            var query = (AccessControlUnitQuery)engine.ReportManager.CreateReportQuery(ReportType.AccessControlUnitActivity);
-            query.Units.AddRange(units);
-            return ExecuteQuery(query, from, to);
-        }
-
-        static async Task<List<AccessControlEvent>> ExecuteQuery(AccessControlReportQuery query, DateTime from, DateTime to)
-        {
-            query.TimeRange.SetTimeRange(from, to);
-            query.MaximumResultCount = 10;
-
-            QueryCompletedEventArgs results = await Task.Factory.FromAsync(query.BeginQuery, query.EndQuery, null);
-            return results.Data.AsEnumerable().Select(CreateAccessControlEvent).ToList();
-
-            AccessControlEvent CreateAccessControlEvent(DataRow row) => new AccessControlEvent
+            async Task<List<AccessControlEvent>> ExecuteAccessControlReportQuery(ReportType reportType, DateTime from, DateTime to, int maxResults, params Guid[] entities)
             {
-                Timestamp = DateTime.SpecifyKind(row.Field<DateTime>(AccessControlReportQuery.TimestampColumnName), DateTimeKind.Utc),
-                Unit = row.Field<Guid?>(AccessControlReportQuery.UnitGuidColumnName),
-                AccessPoint = row.Field<Guid?>(AccessControlReportQuery.APGuidColumnName),
-                AccessPointGroup = row.Field<Guid?>(AccessControlReportQuery.AccessPointGroupGuidColumnName),
-                Credential = row.Field<Guid?>(AccessControlReportQuery.CredentialGuidColumnName),
-                Credential2 = row.Field<Guid?>(AccessControlReportQuery.Credential2GuidColumnName),
-                Device = row.Field<Guid?>(AccessControlReportQuery.DeviceGuidColumnName),
-                CustomEventMessage = row.Field<string>(AccessControlReportQuery.CustomEventMessageColumnName),
-                EventType = row.Field<EventType>(AccessControlReportQuery.EventTypeColumnName),
-                Source = row.Field<Guid?>(AccessControlReportQuery.SourceGuidColumnName),
-                Cardholder = row.Field<Guid?>(AccessControlReportQuery.CardholderGuidColumnName),
-                OccurrencePeriod = row.Field<OfflinePeriodType>(AccessControlReportQuery.OccurrencePeriodColumnName),
-                TimeZone = TimeZoneInfo.FindSystemTimeZoneById(row.Field<string>(AccessControlReportQuery.TimeZoneColumnName))
-            };
+                var query = (AccessControlReportQuery)engine.ReportManager.CreateReportQuery(reportType);
+                query.TimeRange.SetTimeRange(from, to);
+                query.MaximumResultCount = maxResults;
+
+                if (query is VisitorActivityQuery visitorActivityQuery)
+                {
+                    visitorActivityQuery.Visitor = entities.First();
+                }
+                else
+                {
+                    query.QueryEntities.AddRange(entities);
+                }
+
+                QueryCompletedEventArgs results = await Task.Factory.FromAsync(query.BeginQuery, query.EndQuery, null);
+                return results.Data.AsEnumerable().Take(query.MaximumResultCount).Select(CreateAccessControlEvent).ToList();
+
+                AccessControlEvent CreateAccessControlEvent(DataRow row) => new AccessControlEvent
+                {
+                    Timestamp = DateTime.SpecifyKind(row.Field<DateTime>(AccessControlReportQuery.TimestampColumnName), DateTimeKind.Utc),
+                    Unit = row.Field<Guid?>(AccessControlReportQuery.UnitGuidColumnName),
+                    AccessPoint = row.Field<Guid?>(AccessControlReportQuery.APGuidColumnName),
+                    AccessPointGroup = row.Field<Guid?>(AccessControlReportQuery.AccessPointGroupGuidColumnName),
+                    Credential = row.Field<Guid?>(AccessControlReportQuery.CredentialGuidColumnName),
+                    Credential2 = row.Field<Guid?>(AccessControlReportQuery.Credential2GuidColumnName),
+                    Device = row.Field<Guid?>(AccessControlReportQuery.DeviceGuidColumnName),
+                    CustomEventMessage = row.Field<string>(AccessControlReportQuery.CustomEventMessageColumnName),
+                    EventType = row.Field<EventType>(AccessControlReportQuery.EventTypeColumnName),
+                    Source = row.Field<Guid?>(AccessControlReportQuery.SourceGuidColumnName),
+                    Cardholder = row.Field<Guid?>(AccessControlReportQuery.CardholderGuidColumnName),
+                    OccurrencePeriod = row.Field<OfflinePeriodType>(AccessControlReportQuery.OccurrencePeriodColumnName),
+                    TimeZone = TimeZoneInfo.FindSystemTimeZoneById(row.Field<string>(AccessControlReportQuery.TimeZoneColumnName))
+                };
+            }
         }
     }
 }
