f45aec697755c7d115188a8186d50d503f2942b8
Andre Lafleur <alafleur@genetec.com>
2024-08-01 20:09:02 +0800

    Add ControlsSample project and remove deprecated projects

---
+++ ControlsSample/StylesSamplePage.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (C) 2024 by Genetec, Inc. All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+
+namespace Genetec.Dap.CodeSamples
+{
+    using Sdk.Workspace.Pages;
+
+    [Page(typeof(StylesSamplePageDescriptor))]
+    public class StylesSamplePage : Page
+    {
+        public StylesSamplePage() => View = new ControlsSamplePageView();
+    }
+}
