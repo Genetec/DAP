b4d2c02e166ef8c1960309e67d180fbe848f0346
Andre Lafleur <alafleur@genetec.com>
2024-09-19 14:29:38 +0800

    Update to C# 12, enhance docs, and improve database support
    Updated PluginDatabaseSample.csproj to use C# language version 12 and added Microsoft.Data.SqlClient package reference. Enhanced ReadMe.md with detailed documentation and code examples for IPluginDatabaseSupport and related components. Updated SampleDatabaseManager.cs and SamplePlugin.cs to switch to Microsoft.Data.SqlClient, added XML documentation, and implemented new database management methods and event handling. Updated copyright and license information to 2024 and Apache License, Version 2.0.

--- Samples/Plugin SDK/PluginDatabaseSample/SamplePlugin.cs
+++ Samples/Plugin SDK/PluginDatabaseSample/SamplePlugin.cs
@@ -1,19 +1,23 @@
-﻿// Copyright (C) 2023 by Genetec, Inc. All rights reserved.
-// May be used only in accordance with a valid Source Code License Agreement.
+﻿// Copyright 2024 Genetec Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 namespace Genetec.Dap.CodeSamples
 {
     using System;
-    using Sdk.Diagnostics.Logging.Core;
-    using Sdk.EventsArgs;
-    using Sdk.Plugin;
-    using Sdk.Plugin.Interfaces;
-    using Sdk.Plugin.Objects;
+    using Genetec.Sdk.Diagnostics.Logging.Core;
+    using Genetec.Sdk.EventsArgs;
+    using Genetec.Sdk.Plugin;
+    using Genetec.Sdk.Plugin.Interfaces;
+    using Genetec.Sdk.Plugin.Objects;
 
     [PluginProperty(typeof(SamplePluginDescriptor))]
-    public class SamplePlugin : Plugin, IPluginDatabaseSupport
+    public class SamplePlugin : Plugin, IPluginDatabaseSupport // Implement the IPluginDatabaseSupport interface to support database operations
     {
-        private readonly SampleDatabaseManager m_databaseManager = new SampleDatabaseManager();
+        private readonly SampleDatabaseManager m_databaseManager = new();
 
         public DatabaseManager DatabaseManager => m_databaseManager;
 
@@ -27,12 +31,13 @@ namespace Genetec.Dap.CodeSamples
 
         protected override void OnPluginLoaded()
         {
+            // Subscribe to the DatabaseStateChanged event to know when the database is connected
             m_databaseManager.DatabaseStateChanged += (sender, e) =>
             {
                 Logger.TraceDebug($"Database state Changed to {m_databaseManager.State}");
 
-                if (m_databaseManager.State == DatabaseState.Connected)
-                {
+                if (m_databaseManager.State == DatabaseState.Connected) // Insert a log message when the database is connected
+                { 
                     m_databaseManager.InsertLog(DateTime.UtcNow, 1, "This is a test log message.");
                 }
             };
