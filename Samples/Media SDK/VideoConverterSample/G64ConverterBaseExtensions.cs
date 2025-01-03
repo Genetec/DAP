8021e1ece257ff384d82490ed341bebf7ac8ce57
Andre Lafleur <alafleur@genetec.com>
2025-01-03 16:33:46 +0800

    Add VideoConverterSample project

---
+++ Samples/Media SDK/VideoConverterSample/G64ConverterBaseExtensions.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (C) 2023 by Genetec, Inc. All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Genetec.Sdk.Media.Export;
+
+namespace Genetec.Dap.CodeSamples
+{
+    public static class G64ConverterBaseExtensions
+    {
+        public static async Task<List<string>> ConvertAsync(this G64ConverterBase converter, IProgress<(int Percent, string Message)> progress, CancellationToken token)
+        {
+            token.Register(() =>
+            {
+                converter.CancelConversion(true);
+            });
+
+            ConversionFinishedEventArgs args = null;
+
+            converter.ProgressChanged += OnProgressChanged;
+            converter.ConversionFinished += OnConversionFinished;
+            try
+            {
+                await converter.ConvertAsync().ConfigureAwait(false);
+
+                if (args.Result == ConversionResult.Cancelled)
+                    throw new OperationCanceledException(token);
+
+                if (args.ExceptionDetails != null)
+                    throw args.ExceptionDetails;
+
+                if (!string.IsNullOrEmpty(args.ErrorMessage))
+                {
+                    if (args.Result == ConversionResult.PartiallySuccessful)
+                    {
+                        progress.Report((100, args.ErrorMessage));
+                    }
+                    else
+                    {
+                        throw new Exception(args.ErrorMessage);
+                    }
+                }
+
+                return args.Filenames;
+            }
+            finally
+            {
+                converter.ProgressChanged -= OnProgressChanged;
+                converter.ConversionFinished -= OnConversionFinished;
+            }
+
+            void OnProgressChanged(object sender, ProgressChangedEventArgs e) => progress?.Report((e.ProgressPercentage, null));
+
+            void OnConversionFinished(object sender, ConversionFinishedEventArgs e) => args = e;
+        }
+    }
+}
---
+++ Samples/Media SDK/VideoConverterSample/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Genetec.Dap.CodeSamples;
+using Genetec.Sdk.Media.Export;
+using File = System.IO.File;
+
+SdkResolver.Initialize();
+
+string filePath = ReadFilePath();
+await ProcessFile(filePath);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+string ReadFilePath()
+{
+    while (true)
+    {
+        Console.Write("Enter media file path: ");
+        string input = Console.ReadLine();
+        if (File.Exists(input))
+        {
+            string extension = Path.GetExtension(input).ToLower();
+            if (extension is ".g64" or ".g64x")
+            {
+                return input;
+            }
+            Console.WriteLine("Invalid file type. Please enter a .g64, or .g64x file.");
+        }
+        else
+        {
+            Console.WriteLine("File not found. Please enter a valid file path.");
+        }
+    }
+}
+
+async Task ProcessFile(string filePath)
+{
+    var cancellationTokenSource = new CancellationTokenSource();
+
+    _ = Task.Run(() =>
+    {
+        Console.WriteLine("Press any key to cancel the conversion...");
+        Console.ReadKey(true);
+        cancellationTokenSource.Cancel();
+    });
+
+    Console.WriteLine("Converting to MP4...");
+    try
+    {
+        string convertedFile = await ConvertToMp4(filePath, true, new Progress<(int Percent, string Message)>(ReportProgress), cancellationTokenSource.Token);
+        Console.WriteLine($"\nConversion to MP4 completed: {convertedFile}");
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine("\nMP4 conversion cancelled.");
+        return;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"MP4 conversion failed: {ex.Message}");
+    }
+
+    Console.WriteLine("\nConverting to ASF...");
+    try
+    {
+        string convertedFile = await ConvertToAsp(filePath, true, new Progress<(int Percent, string Message)>(ReportProgress), cancellationTokenSource.Token);
+        Console.WriteLine($"\nConversion to ASF completed: {convertedFile}");
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine("\nASF conversion cancelled.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"ASF conversion failed: {ex.Message}");
+    }
+
+    void ReportProgress((int Percent, string Message) progress)
+    {
+        Console.Write($"\rConversion progress: {progress.Percent}%");
+        if (!String.IsNullOrEmpty(progress.Message))
+        {
+            Console.Write($"\n{progress.Message}");
+        }
+    }
+}
+
+async Task<string> ConvertToMp4(string filePath, bool exportAudio = true, IProgress<(int Percent, string Message)> progress = default, CancellationToken token = default)
+{
+    using var converter = new G64ToMp4Converter();
+
+    converter.Initialize(
+        filePath: filePath,
+        exportAudio: exportAudio,
+        outputFilePath: Path.ChangeExtension(filePath, ".mp4"));
+
+    return (await converter.ConvertAsync(progress, token)).FirstOrDefault();
+}
+
+async Task<string> ConvertToAsp(string filePath, bool exportAudio = true, IProgress<(int Percent, string Message)> progress = default, CancellationToken token = default)
+{
+    using var converter = new G64ToAsfConverter();
+
+    converter.Initialize(
+        filePath: filePath,
+        displayDateTime: false,
+        writeCameraName: false,
+        exportAudio: exportAudio,
+        outputFilePath: Path.ChangeExtension(filePath, ".asf"),
+        profileId: G64ToAsfConverter.GetAsfProfiles().First().Profile);
+
+    return (await converter.ConvertAsync(progress, token)).FirstOrDefault();
+}
