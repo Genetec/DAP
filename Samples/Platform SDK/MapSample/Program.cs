ce37ae7155f64e4944fb600ab1dab709ae8e7bee
Andre Lafleur <alafleur@genetec.com>
2025-06-23 14:17:08 +0800

    Add MapSample project with initial configuration and logic
    # Motivation
    This commit introduces the "MapSample" project to demonstrate the use of
    the Genetec SDK for creating maps and managing client certificates.
    
    # Modifications
    * Added a new project "MapSample" to the solution file `Genetec.Dap.CodeSamples.sln`.
    * Created `App.config` with basic XML configuration settings for the app.
    * Added client certificate configuration in `MapSample.exe.cert` for company
      and application details.
    * Created `MapSample.csproj` with build configurations, library references,
      and a target for copying certificates.
    * Added binary file `OfficeFloor.png` to be used as a resource in the project.
    * Created `Program.cs` containing the main application logic for SDK
      initialization, server login, and map creation.

---
+++ Samples/Platform SDK/MapSample/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2024 Genetec Inc.
+// Licensed under the Apache License, Version 2.0
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Genetec.Dap.CodeSamples;
+using Genetec.Sdk;
+using Genetec.Sdk.Entities;
+
+SdkResolver.Initialize();
+
+await RunSample();
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(true);
+
+async Task RunSample()
+{
+    const string server = "localhost";
+    const string username = "admin";
+    const string password = "";
+
+    using var engine = new Engine();
+
+    ConnectionStateCode state = await engine.LogOnAsync(server, username, password);
+
+    if (state != ConnectionStateCode.Success)
+    {
+        Console.WriteLine($"Logon failed: {state}");
+        return;
+    }
+
+    var filePath = Path.Combine(Environment.CurrentDirectory, "OfficeFloor.png");
+
+    var officeArea = (Area)engine.CreateEntity("Office", EntityType.Area);
+    Map officeMap = officeArea.CreateMap(new DocumentMapCreationInfo(filePath: filePath, documentType: MapDocumentTypes.Image, zoomLevel: 1));
+
+    var singaporeArea = (Area)engine.CreateEntity("Singapore", EntityType.Area);
+    Map singaporeMap = officeArea.CreateMap(new GeographicalMapCreationInfo(MapProviders.Bing));
+    singaporeMap.DefaultView = new GeoView(new GeoCoordinate(1.3521, 103.8198, 15.0), 1);
+}
