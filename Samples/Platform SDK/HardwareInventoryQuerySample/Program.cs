b36c9b29f5ceeffcd5bf67bd1abdb6c7d46b1f8e
Andre Lafleur <alafleur@genetec.com>
2025-09-08 11:01:22 +0800

    Add HardwareInventoryQuerySample project to solution
    This commit introduces a new project for querying hardware inventory,
    along with necessary configurations and implementations.
    
    # Motivation
    To provide a sample application that demonstrates how to retrieve and
    display hardware inventory information using the Genetec SDK.
    
    # Modifications
    * Added a new project called "HardwareInventoryQuerySample" to the solution.
    * Updated the solution configuration to include build settings for the new project.
    * Introduced a new `HardwareInventoryInfo` record for encapsulating hardware details.
    * Implemented the `HardwareInventoryQuerySample` class for retrieving hardware info.
    * Added XML configuration for the application, specifying the supported runtime.
    * Included a client certificate configuration for the hardware inventory sample.
    * Updated the project file for `HardwareInventoryQuerySample` with properties and references.
    * Modified `Program.cs` to create and run an instance of `HardwareInventoryQuerySample`.
    * Added copyright and licensing information to several files for Genetec Inc.

---
+++ Samples/Platform SDK/HardwareInventoryQuerySample/Program.cs
@@ -0,0 +1,7 @@
+// Copyright 2025 Genetec Inc.
+// Licensed under the Apache License, Version 2.0
+
+using Genetec.Dap.CodeSamples;
+
+var sample = new HardwareInventoryQuerySample();
+await sample.RunAsync();
